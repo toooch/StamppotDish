8d96e7bfd0b3734af68352e4e1d7f447aae87275
DirkEz <dirk.schaafstra@gmail.com>
2023-02-28 17:09:55 +0100

    Added Mod files

---
+++ StamppotDishes/Main.cs
@@ -0,0 +1,57 @@
+﻿using StamppotDishes.Customs;
+
+using Kitchen;
+using KitchenLib.References;
+using KitchenLib;
+using KitchenLib.Event;
+using KitchenMods;
+using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using KitchenLib.Colorblind;
+
+using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Reflection;
+
+using IngredientLib.Util;
+
+using static KitchenData.ItemGroup;
+using static KitchenLib.Utils.GDOUtils;
+using static KitchenLib.Utils.KitchenPropertiesUtils;
+using TMPro;
+using Object = UnityEngine.Object;
+
+namespace StamppotDishes
+{
+    public class Mod : BaseMod, IModSystem
+    {
+        // GUID must be unique and is recommended to be in reverse domain name notation
+        // Mod Name is displayed to the player and listed in the mods menu
+        // Mod Version must follow semver notation e.g. "1.2.3"
+        public const string MOD_GUID = "Tooch.PlateUp.StamppotDishes";
+        public const string MOD_NAME = "Stamppot Dishes";
+        public const string MOD_VERSION = "0.0.1";
+        public const string MOD_AUTHOR = "Tooch";
+        public const string MOD_GAMEVERSION = ">=1.1.4";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.3" current and all future
+        // e.g. ">=1.1.3 <=1.2.3" for all from/until
+
+        public static AssetBundle Bundle;
+
+        public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        private void AddGameData()
+        {
+            // Hutspot
+            AddGameDataObject<>();
+
+        }
+    }
+}
---
+++ StamppotDishes/Mains/StamppotDish.cs
@@ -0,0 +1,41 @@
+﻿using KitchenData;
+using KitchenLib.Customs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace StamppotDishes.Mains
+{
+    class StamppotDish : CustomDish
+    {
+        public override string UniqueNameID => "Stamppot";
+        public override DishType Type => DishType.Base;
+        public override GameObject DisplayPrefab => Mod.Bundle.LoadAsset<GameObject>("StamppotIcon");
+        public override GameObject IconPrefab => DisplayPrefab;
+        public override DishCustomerChange CustomerMultiplier => DishCustomerChange.LargeDecrease;
+        public override CardType CardType => CardType.Default;
+        public override Unlock.RewardLevel ExpReward => Unlock.RewardLevel.Small;
+        public override UnlockGroup UnlockGroup => UnlockGroup.Dish;
+        public override bool IsSpecificFranchiseTier => false;
+        public override bool IsAvailableAsLobbyOption => true;
+        public override bool DestroyAfterModUninstall => false;
+        public override bool IsUnlockable => true;
+
+        public override List<string> StartingNameSet => new List<string>
+        {
+            "Lick the Stamp and send it",
+            "Stamp it"
+        };
+
+        public override List<Dish.MenuItem> ResultingMenuItems => new List<Dish.MenuItem>
+        {
+            new Dish.MenuItem
+            {
+                
+            }
+        }
+    }
+}
